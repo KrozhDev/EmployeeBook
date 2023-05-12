0ea1ea7ad837125f521764a521cf60ad8c603cfe
KrozhDev <roko.mdn@gmail.com>
2023-05-12 12:51:50 +0300

    medium complete

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,21 @@ public class Main {
         System.out.println(averageSalary());
         printFIO();
 
+        indexSalary(10);
+        printEmployees();
+
+        System.out.println(minSalaryDepartment(Department.FIRST));
+        System.out.println(maxSalaryDepartment(Department.FIRST));
+        System.out.println(totalSalaryDepartment(Department.FIRST));
+        System.out.println(averageSalaryDepartment(Department.FIRST));
+
+        indexSalaryDepartment(Department.FIRST, 10);
+
+        printEmployeesDepartment(Department.FIRST);
+
+        printWithSalaryLessThan(150000);
+
+
     }
 
     public static void printEmployees() {
@@ -84,6 +99,102 @@ public class Main {
         for (Employee emp : employees) {
             if (emp != null) {
                 System.out.println(emp.getSurname() + " " + emp.getName() + " " + emp.getFatherName());
+            }
+        }
+    }
+
+    public static void indexSalary(double percent) {
+        double index = 1 + percent/100;
+        for (Employee emp : employees) {
+            if (emp != null) {
+                emp.setSalary((int) (emp.getSalary() * index));
+            }
+        }
+    }
+
+    public static Employee minSalaryDepartment(Department department) {
+        int minSalary = Integer.MAX_VALUE;
+        Employee employeeWithMinSalary = null;
+        for (Employee emp : employees) {
+            if (emp != null) {
+                if (emp.getDepartment() == department) {
+                    if (emp.getSalary() < minSalary) {
+                        minSalary = emp.getSalary();
+                        employeeWithMinSalary = emp;
+                    }
+                }
+            }
+        }
+        return employeeWithMinSalary;
+    }
+
+    public static Employee maxSalaryDepartment(Department department) {
+        int maxSalary = 0;
+        Employee employeeWithMaxSalary = null;
+        for (Employee emp : employees) {
+            if (emp != null) {
+                if (emp.getDepartment() == department) {
+                    if (emp.getSalary() > maxSalary) {
+                        maxSalary = emp.getSalary();
+                        employeeWithMaxSalary = emp;
+                    }
+                }
+            }
+        }
+        return employeeWithMaxSalary;
+    }
+
+    public static int totalSalaryDepartment(Department department) {
+        int total = 0;
+        for (Employee emp : employees) {
+            if (emp != null) {
+                if (emp.getDepartment() == department) {
+                    total += emp.getSalary();
+                }
+            }
+        }
+        return total;
+    }
+
+    public static double averageSalaryDepartment(Department department) {
+        int counter = 0;
+        for (Employee emp : employees) {
+            if (emp != null) {
+                if (emp.getDepartment() == department) {
+                    counter++;
+                }
+            }
+        }
+        return totalSalaryDepartment(department)/counter;
+    }
+
+    public static void indexSalaryDepartment(Department department, double percent) {
+        double index = 1 + percent / 100;
+        for (Employee emp : employees) {
+            if (emp != null) {
+                if (emp.getDepartment() == department) {
+                    emp.setSalary((int) (emp.getSalary() * index));
+                }
+            }
+        }
+    }
+
+    public static void printEmployeesDepartment(Department department) {
+        for (Employee emp : employees) {
+            if (emp != null) {
+                if (emp.getDepartment() == department) {
+                    System.out.println(emp.getSurname() + " " + emp.getName() + " " + emp.getFatherName());
+                }
+            }
+        }
+    }
+
+    public static void printWithSalaryLessThan(int salary) {
+        for (Employee emp: employees) {
+            if (emp != null) {
+                if (emp.getSalary() < salary) {
+                    System.out.println(emp.toString());
+                }
             }
         }
     }
